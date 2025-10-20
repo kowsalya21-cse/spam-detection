37e8158c3ccf2dbbf092232ff8b5afc1233463e0
kowsalya21-cse <kowsalya76800@gmail.com>
2025-10-20 11:12:20 +0530

    Update App.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// client/src/App.js
+import React from 'react';
+import SubmitForm from './components/SubmitForm';
+
+function App(){
+  return (
+    <div>
+      <h2 style={{textAlign:'center'}}>SMS Spam Detection UI</h2>
+      <SubmitForm />
+    </div>
+  );
+}
+export default App;
+
